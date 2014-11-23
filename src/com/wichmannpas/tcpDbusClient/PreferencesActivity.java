995e688491285df485999fd11581d691b2b1b184
Pascal Wichmann <wichmannpas@gmail.com>
2014-11-23 14:29:17 +0100

    feature: preferences can be saved

--- src/com/wichmannpas/tcpDbusClient/PreferencesActivity.java
+++ src/com/wichmannpas/tcpDbusClient/PreferencesActivity.java
@@ -1,12 +1,32 @@
 package com.wichmannpas.tcpDbusClient;
 
 import android.content.SharedPreferences;
+import android.content.SharedPreferences.Editor;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
+import android.view.View;
 import android.widget.EditText;
 
 public class PreferencesActivity extends ActionBarActivity {
 
+	public void savePreferences(View view)
+	{
+		//load SharedPreferences instance
+		SharedPreferences preferences = getSharedPreferences("preferences", MODE_PRIVATE);
+
+		//get values of inputs
+		EditText tcpHost = (EditText)findViewById(R.id.tcpHost);
+		EditText tcpPort= (EditText)findViewById(R.id.tcpPort);
+		EditText target = (EditText)findViewById(R.id.target);
+		
+		//set user values
+    	Editor editor = preferences.edit();
+    	editor.putString("host", tcpHost.getText().toString());
+    	editor.putInt("port", Integer.valueOf(tcpPort.getText().toString()));
+    	editor.putString("target", target.getText().toString());
+    	editor.commit();
+	}
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -17,7 +37,7 @@ public class PreferencesActivity extends ActionBarActivity {
 		EditText tcpPort= (EditText)findViewById(R.id.tcpPort);
 		EditText target = (EditText)findViewById(R.id.target);
 		tcpHost.setText(preferences.getString("host", ""));
-        tcpPort.setText(preferences.getString("target", "spotify"));
-    	target.setText(String.valueOf(preferences.getInt("port", 12341)));
+		tcpPort.setText(String.valueOf(preferences.getInt("port", 12341)));
+        target.setText(preferences.getString("target", "spotify"));
 	}
 }
